004ca486fad403799fe1e4e9a2ce76156b638123
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-03 19:50:40 +0530

    Update simpleintrest.py

--- simpleintrest.py
+++ simpleintrest.py
@@ -1,5 +1,4 @@
 import sys
-
 if len(sys.argv) == 4:
     principal = sys.argv[1]
     rate = sys.argv[2]
@@ -10,9 +9,7 @@ else:
     principal = "10000"
     rate = "18"
     time = "2"
-
 simple_interest = (eval(principal) * eval(rate) * eval(time)) / 100
-
 print("Principal Amount:", principal)
 print("Rate of Interest:", rate)
 print("Time Period (years):", time)
